9c7bf2d463db86369c709cd5113cc0cb4e4b663f
Carlos Perdigão <carlos.perdigao@outsystems.com>
2024-12-26 11:35:16 +0000

    Updated to NetCore 8.0

--- AsposeWordsUnitTesting/EmptyLicenseTest.cs
+++ AsposeWordsUnitTesting/EmptyLicenseTest.cs
@@ -11,13 +11,17 @@ namespace AsposeWordsUnitTesting
         {
             AsposeWords aw = new AsposeWords();
             AsposeResult docRes, docxRes;
-            byte[] doc = System.IO.File.ReadAllBytes("./Resources/OutSystemsDoc.Doc"); 
+            byte[] doc = System.IO.File.ReadAllBytes("./Resources/OutSystemsDoc.Doc");
+            if (doc.Length == 0)
+                Assert.Fail();
             docRes = aw.DocToPdf(new byte[] { }, doc);
             Assert.IsTrue(docRes.success);
             System.IO.File.WriteAllBytes("./Resources/OutSystemsDoc.pdf", docRes.document);
             
 
-            byte[] docx = System.IO.File.ReadAllBytes("./Resources/OutSystemsDocX.Docx"); 
+            byte[] docx = System.IO.File.ReadAllBytes("./Resources/OutSystemsDocX.Docx");
+            if (docx.Length == 0)
+                Assert.Fail();
             docxRes = aw.DocToPdf(new byte[] { }, docx);
             Assert.IsTrue(docxRes.success);
             System.IO.File.WriteAllBytes("./Resources/OutSystemsDocX.pdf", docxRes.document);
@@ -30,7 +34,8 @@ namespace AsposeWordsUnitTesting
             AsposeWords aw = new AsposeWords();
             AsposeResult docRes;
             byte[] doc = System.IO.File.ReadAllBytes("./Resources/OutSystemsMailMerge.Dotx"); ;
-            
+            if (doc.Length == 0 )
+                Assert.Fail();
             docRes = aw.MailMergeDoc(new byte[] { }, doc, "NAME,COMPANY","CARLOS PERDIGÃO,OUTSYSTEMS");
             Assert.IsTrue(docRes.success);
             System.IO.File.WriteAllBytes("./Resources/OutSystemsMerge.docx", docRes.document);
@@ -43,7 +48,9 @@ namespace AsposeWordsUnitTesting
             AsposeWords aw = new AsposeWords();
             AsposeResult docRes;
             byte[] doc = System.IO.File.ReadAllBytes("./Resources/OutSystemsImage.Docx"); 
-            byte[] pic = System.IO.File.ReadAllBytes("./Resources/odc.png"); ;
+            byte[] pic = System.IO.File.ReadAllBytes("./Resources/odc.png");
+            if(doc.Length == 0 || pic.Length == 0)
+               Assert.Fail();
             docRes = aw.InsertImage(new byte[] { }, doc,"SecondPic",pic);
             Assert.IsTrue(docRes.success);
             System.IO.File.WriteAllBytes("./Resources/OutSystemsPic.docx", docRes.document);
